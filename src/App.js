3c9daf8a48de785e81ea81903b94f58ae6113277
BigyanH <him410935@gmail.com>
2025-08-16 18:25:23 +0545

    netlify connect redirect added

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Home from "./components/Home";
 import About from "./components/About";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
-import "./App.css";
 
 function App() {
   return (
