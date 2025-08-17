20d1b4c4d0e6cc4d8fb4f72b22007e3d3a1b193c
Bigyan Himalaya <him410935@gmail.com>
2025-08-17 07:36:26 +0545

    pepe yo added

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,7 @@ function Home() {
   return (
     <div className="home">
         <div className="home-container">
-        <img src="/pepe.webp" alt="Bigyan Himalaya" className="portrait" />
+        <img src="/pepe yo.jpg" alt="Bigyan Himalaya" className="portrait" />
         <div className="home-text">
           <h1>Hello, I'm <span>Bigyan Himalaya</span></h1>
       <p>Graphic designer | 3D modeling Enthusiast | React Enthusiast 🚀</p>
